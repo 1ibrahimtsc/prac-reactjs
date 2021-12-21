120ad88462a17512bd7b8a586bfd76c553e922be
1ibrahimtsc <1ibrahimtsc@gmail.com>
2021-12-21 21:44:20 +0600

    HTTP Get Request by axios

--- src/App.js
+++ src/App.js
@@ -1,19 +1,9 @@
-//import React, { useReducer } from 'react';
-//import Count from './components/Counter';
-//import CounterTwo from './components/CounterTwo';
-//import CounterThree from './components/CounterThree';
-//import ComponentA from './components/ComponentA';
-//import ComponentB from './components/ComponentB';
-import CompOne from './components/CompOne';
-import { UserProvider } from './components/UserContext';
-
+import Posts from './components/Posts';
 
 function App() {  
  return (
    <div>       
-       <UserProvider value="John">
-       <CompOne />
-        </UserProvider>
+       <Posts />
    </div>
  )
 }
