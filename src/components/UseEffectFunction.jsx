bb114e6c5c8da1a5825ab719aeb7227475b72abe
1ibrahimtsc <1ibrahimtsc@gmail.com>
2021-12-23 07:23:29 +0600

    useEffect run conditionally

--- src/components/UseEffectFunction.jsx
+++ src/components/UseEffectFunction.jsx
@@ -2,15 +2,22 @@ import React, { useState, useEffect } from 'react';
 
 const UseEffectFunction = ()=> {
     const [count, setCount] = useState(0)
+    const [name, setName] = useState("")
+
 
     useEffect(()=>{
+        console.log("useEffect Run")
         document.title = `count ${count}`
-    })
+    }, [count])
 
     return(
         <div>
-            <h1> Count : {count} from hook counter </h1>
-            <button onClick={()=> setCount(count + 1)  } >Increment</button>
+            <h1> Conditionally run useEffect </h1>
+            <input type="text" value={name} onChange={e => setName(e.target.value)} />
+            <h2>Name is : {name}</h2>
+            <h3>Count: {count}</h3>
+            <button onClick={()=> setCount(count + 2)}>Increment</button>
+            
         </div>
     )
 }
